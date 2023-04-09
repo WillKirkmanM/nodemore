27683e7b4619e2f1e00e366262e5a085734419e9
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-09 23:22:37 +0100

    Feat(main.rs): Implementation of Should Clean Directory

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,9 @@
 mod time;
 mod config;
+mod dir;
 
-use time::{
-    get_unix_last_modified,
-    human_to_unix_time
-};
-use config::read_config_file; 
+use dir::should_clean_dir;
 
 fn main() {
-    /*
-    match get_last_modified("farm/index.js") {
-        Ok(time) => println!("{time:?}"),
-        Err(error) => eprintln!("{}", error),
-    }
-    */
-
-    // let config = read_config_file().unwrap();
-    // println!("{}", config.cleaning.time)
-    
-    human_to_unix_time("1 month");
+    dbg!(should_clean_dir("farm"));
 }
