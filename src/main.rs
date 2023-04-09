a640f28068b641bb1358b330834c33b1c7a1ae3b
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-09 22:09:36 +0100

    Feat(main.rs): Implementation of Time Functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod time;
+mod config;
+
+use time::{
+    get_unix_last_modified,
+    human_to_unix_time
+};
+use config::read_config_file; 
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    match get_last_modified("farm/index.js") {
+        Ok(time) => println!("{time:?}"),
+        Err(error) => eprintln!("{}", error),
+    }
+    */
+
+    // let config = read_config_file().unwrap();
+    // println!("{}", config.cleaning.time)
+    
+    human_to_unix_time("1 month");
 }
