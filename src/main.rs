fe7d26662a363c977b953894032bfa6d1caca71b
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-10 23:26:43 +0100

    Feat(main.rs): Implementation of Nodemore

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@ use dir::check_dir;
 use args::NodemoreArgs;
 use clap::Parser;
 
+use config::read_config_file;
+use colored::Colorize;
+
 fn main() {
-    check_dir("../../Projects")
+
+    let config = read_config_file().unwrap();
+    println!("NodeJS Projects Not Accessed in the last {}:", (config.cleaning.time).bright_green());
+    let count = check_dir("../../Projects");
+    println!("Do you want to clean these {} Projects?", count.to_string().green())
 
     /*
     if should_clean_dir("Projects") {
