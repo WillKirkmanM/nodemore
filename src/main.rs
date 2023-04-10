371471e6e1dd6c9eb3227bda075b54568e8bff6b
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-10 15:08:37 +0100

    Feat(main.rs): Implementation of Check Dir

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod time;
 mod config;
 mod dir;
+mod args;
 
-use crate::dir::should_clean_dir;
+use dir::check_dir;
+use args::NodemoreArgs;
+use clap::Parser;
 
 fn main() {
+    check_dir("../../Projects")
+
+    /*
     if should_clean_dir("Projects") {
         println!("damn")
     } else {
         println!("cool u good")
     };
+    */
 }
