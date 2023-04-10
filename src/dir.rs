955e6b8476d8327a9d1a7c68fe461dc48487e2a7
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-10 15:09:27 +0100

    Feat(dir.rs): Implemented Colour

--- src/dir.rs
+++ src/dir.rs
@@ -1,13 +1,69 @@
 use std::fs;
 use std::path::Path;
+use std::io::stdin;
 
-use crate::time::{
+use crate::{time::{
     human_to_unix_time,
     get_unix_last_modified
-};
+}};
+use crate::args::NodemoreArgs;
+
+use colored::Colorize;
+use clap::Parser;
+
+pub fn contains_only_folders(dir: &str) -> bool {
+    let contents = fs::read_dir(dir).unwrap();
+    for file in contents {
+        let file = file.unwrap();
+        if file.file_type().unwrap().is_file() {
+            return false;
+        }
+    }
+    true
+}
 
 pub fn check_dir(dir: &str) {
-}
+    let dir_list = fs::read_dir(dir).unwrap();
+
+    for directory in dir_list {
+        let directory = directory.unwrap();
+        let dir_path = &directory.path();
+        let dir_path_str = dir_path.to_str().unwrap();
+        let tmp = &directory.file_name();
+        let dir_name = tmp.to_str().unwrap();
+
+        let is_dir = fs::metadata(dir_path).unwrap().is_dir();
+        if is_dir && contains_only_folders(dir_path_str) {
+            check_dir(dir_path_str)
+        }
+        if is_dir {
+            if Path::new(&(dir_path.to_str().unwrap().to_string() + "/" + "package.json")).try_exists().unwrap() {
+                // println!("{:?} is a node project", dir_path);
+                let should = should_clean_dir_no_checks(dir_path_str);
+                let args = NodemoreArgs::parse();
+                if should {
+                    if args.prompt {
+                        println!("Would you like to {} {}?", "clean".red(), dir_name.bright_green());
+
+                        let mut input = String::new();
+                        stdin().read_line(&mut input).unwrap();
+                        input = input.trim().to_string();
+
+                        dbg!(&input);
+                        if input == "Y" || input == "y" {
+                            todo!("Delete Node Modules")
+                        } else {
+                            continue
+                        }
+                    } else {
+                        println!("[{}]: {} is gone", "-".red(), dir_name.bright_green())
+                    }
+                } 
+            } 
+        }
+    }
+} 
+
 
 pub fn should_clean_dir(dir: &str) -> bool {
     let node_modules_exists = Path::new(&(dir.to_owned() + "/" + "node_modules")).exists();
@@ -22,15 +78,18 @@ pub fn should_clean_dir(dir: &str) -> bool {
             if is_dir && file_name == "node_modules" {
                 continue;
             } else if is_dir {
-                should_clean_dir_no_checks(file_path.clone().to_str().unwrap());
+                let should = should_clean_dir(file_path.clone().to_str().unwrap());
+                if should {
+                    println!("{:?} Should be deleted", file_name)
+                }
                 continue;
             } else {
-                println!("File {:?}", &file_path);
+                // println!("File {:?}", &file_path);
                 let file_path_str = file_path.to_str().unwrap();
 
                 let should = should_clean_file(file_path_str).unwrap();
                 if should == false {
-                    // return false
+                    return false
                 }
             };
         }
@@ -48,15 +107,18 @@ pub fn should_clean_dir_no_checks(dir: &str) -> bool {
 
             let is_dir = fs::metadata(&file_path).unwrap().is_dir();
             if is_dir {
-                should_clean_dir(file_path.clone().to_str().unwrap());
+                let should = should_clean_dir(file_path.clone().to_str().unwrap());
+                if should {
+                    println!("{:?} Should be deleted", file_name)
+                }
                 continue;
             } else {
-                println!("File {:?}", &file_path);
+                // println!("File {:?}", &file_path);
                 let file_path_str = file_path.to_str().unwrap();
 
                 let should = should_clean_file(file_path_str).unwrap();
                 if should == false {
-                    // return false
+                    return false
                 }
             };
         }
