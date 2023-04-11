81513b0d4b4675d01d15ddcb6c0ceb0c05d02cd2
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-11 17:00:15 +0100

    Feat(main.rs): Implementation of Init Function

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,8 @@ mod config;
 mod dir;
 mod args;
 
-use dir::check_dir;
-use args::NodemoreArgs;
-use clap::Parser;
-
-use config::read_config_file;
-use colored::Colorize;
+use crate::dir::init;
 
 fn main() {
-
-    let config = read_config_file().unwrap();
-    println!("NodeJS Projects Not Accessed in the last {}:", (config.cleaning.time).bright_green());
-    let count = check_dir("../../Projects");
-    println!("Do you want to clean these {} Projects?", count.to_string().green())
-
-    /*
-    if should_clean_dir("Projects") {
-        println!("damn")
-    } else {
-        println!("cool u good")
-    };
-    */
+    init()
 }
