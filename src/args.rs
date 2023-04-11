1f53510ef5e11783d95c14e7b6b37e0965eddbe1
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-11 18:43:19 +0100

    Feat(args.rs): Added Path & Time Arguments

--- src/args.rs
+++ src/args.rs
@@ -3,14 +3,20 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct NodemoreArgs {
+    /// Path to Search
+    #[clap(short, long, default_value = ".")]
+    pub path: String,
+
+    /// Time Frame
+    #[clap(short, long, default_value = "1 week")]
+    pub time: String,
+
     /// Prompt Before Deletion 
-    #[clap(short, long)]
+    #[arg(short = 'a', long)]
     pub prompt: bool,
 
     /// Verbosity Level
     #[clap(short, long, default_value = "0")]
     pub verbosity: i32
-
 }
-
 
