11169d19a8d577add07aab42de9eef62de0ce8cf
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-12 19:30:11 +0100

    Feat(args.rs): Added show_size Argument

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 
+use crate::config::read_config_file;
+
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct NodemoreArgs {
@@ -15,8 +17,57 @@ pub struct NodemoreArgs {
     #[arg(short = 'a', long)]
     pub prompt: bool,
 
+    /// Whether to show file sizes (Slow)
+    #[arg(short = 's', long)]
+    pub show_size: bool,
+
     /// Verbosity Level
     #[clap(short, long, default_value = "0")]
     pub verbosity: i32
 }
+
+use crate::config::{
+    Config,
+    CleaningConfig
+};
+
+fn get_blank_config() -> Config {
+    let blank_cleaning = CleaningConfig {
+        time: String::new(),
+        path: String::new()
+    };
+    let blank = Config {
+        cleaning: blank_cleaning
+    };
+
+    blank
+}
+
+pub fn get_cleaning_time() -> String {
+    let args = NodemoreArgs::parse();
+    let config = read_config_file().unwrap_or(get_blank_config());
+    
+    if args.time != "" {
+        args.time
+    } else if config.cleaning.time != "" {
+        config.cleaning.time
+    } else {
+        String::from("1 Day")
+    }
+}
+
+pub fn get_cleaning_path() -> String {
+    let args = NodemoreArgs::parse();
+    let config = read_config_file().unwrap_or(get_blank_config());
+    
+    if args.path != "" {
+        args.path
+    } else if config.cleaning.path != "" {
+        config.cleaning.path
+    } else {
+        String::from(".")
+    }
+}
+
+
 
